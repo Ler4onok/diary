d75af942cabcc1a68c6949a594f10623fd1a4479
Valeryia Chyzhova <chyzhval@fel.cvut.cz>
2020-06-07 14:51:49 +0200

    Updates

---
+++ precache-manifest.5ac14d2025fdeb79190a1a2cd59b013c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f610ae740c6de2d9d92d0fac1f9f911",
+    "url": "/diary/index.html"
+  },
+  {
+    "revision": "224c9f907e1946ae9b08",
+    "url": "/diary/static/css/main.ea101b5c.chunk.css"
+  },
+  {
+    "revision": "ff3cf2775b0cc47d644a",
+    "url": "/diary/static/js/2.12b2b500.chunk.js"
+  },
+  {
+    "revision": "1599112f5867042ef505091e0fb03a4e",
+    "url": "/diary/static/js/2.12b2b500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224c9f907e1946ae9b08",
+    "url": "/diary/static/js/main.9e517619.chunk.js"
+  },
+  {
+    "revision": "a31e60c14d35c6097fc2",
+    "url": "/diary/static/js/runtime-main.96268394.js"
+  },
+  {
+    "revision": "64c19fffd4e8e12e79b5ef8a75c01e80",
+    "url": "/diary/static/media/August.64c19fff.ttf"
+  },
+  {
+    "revision": "ae2a95d99bb298c15e31923cd55ee043",
+    "url": "/diary/static/media/Muli-ExtraBold.ae2a95d9.ttf"
+  },
+  {
+    "revision": "f9b7a534f6b6a444ad62ea7e2df39303",
+    "url": "/diary/static/media/Muli-ExtraLight.f9b7a534.ttf"
+  },
+  {
+    "revision": "fb56401f2c9be89c17136b3f1264447c",
+    "url": "/diary/static/media/Muli-Light.fb56401f.ttf"
+  },
+  {
+    "revision": "9df848b4aeeabe281ed7d59b65abcb54",
+    "url": "/diary/static/media/add-image.9df848b4.png"
+  },
+  {
+    "revision": "0cc96624d57845b08aaad49f99e079fa",
+    "url": "/diary/static/media/addProfileImg.0cc96624.png"
+  },
+  {
+    "revision": "bfd4a238f890dbcd471a4e0f888d068c",
+    "url": "/diary/static/media/add_post.bfd4a238.jpg"
+  },
+  {
+    "revision": "828974d5944987c1bb0502089704d1bd",
+    "url": "/diary/static/media/background.828974d5.jpg"
+  },
+  {
+    "revision": "e00109acdaa6f25b61f6156b8810fee6",
+    "url": "/diary/static/media/deletePost.e00109ac.svg"
+  },
+  {
+    "revision": "d85728240399e527eff794efbb745499",
+    "url": "/diary/static/media/lock.d8572824.svg"
+  },
+  {
+    "revision": "e33ff0763c08371470fedb3d310c3b2b",
+    "url": "/diary/static/media/login-1.e33ff076.png"
+  },
+  {
+    "revision": "011b04722550f2a907277370aa2682e3",
+    "url": "/diary/static/media/profile.011b0472.jpg"
+  },
+  {
+    "revision": "dbfa8ea2b89c27321f22729899767e6d",
+    "url": "/diary/static/media/profileImg.dbfa8ea2.png"
+  }
+]);
--- src/components/ModalWindowLogIn/BasicSchema.js
+++
@@ -1,8 +0,0 @@
-import * as Yup from "yup";
-
-const BasicSchema = Yup.object().shape({
-  username: Yup.string().required("Username is required"),
-  password: Yup.string().required("Password is required"),
-});
-
-export default BasicSchema;
