afc9cdce8690aa946d729c2d3f19b0267175fa6e
Valeryia Chyzhova <chyzhval@fel.cvut.cz>
2020-06-07 15:04:14 +0200

    fix route for deploy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import {Routes} from "./Routes";
+import { Routes } from "./Routes";
 import { BrowserRouter } from "react-router-dom";
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes />
     </BrowserRouter>
   </React.StrictMode>,
